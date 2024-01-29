77cfd0ea23f2ca7b1c9392e65a65149f08116028
Carifio24 <carifio24@gmail.com>
2024-01-29 11:45:11 -0500

    Finish conversion from ra/dec -> lb.

--- convert.py
+++ convert.py
@@ -1,4 +1,4 @@
-from os.path import extsep, splitext
+from os.path import extsep, join, splitext
 
 import pandas as pd
 import astropy.units as u
@@ -10,14 +10,14 @@ def convert_coordinates(df):
     out["phase"] = df["phase"]
     out["d"] = df["d"]
     ra_dec = zip(df["ra"], df["dec"])
-    coords = [SkyCoord(ra=ra * u.deg, dec=dec * u.deg) for ra, dec in ra_dec]
+    coords = [SkyCoord(ra=ra * u.deg, dec=dec * u.deg).transform_to(Galactic) for ra, dec in ra_dec]
     ls = []
     bs = []
     for coord in coords:
-        ls.append(coord.icrs.ra.value)
-        decs.append(coord.icrs.dec.value)
-    out["ra"] = ras
-    out["dec"] = decs
+        ls.append(coord.l.value)
+        bs.append(coord.b.value)
+    out["l"] = ls 
+    out["b"] = bs 
 
     return out
 
@@ -26,12 +26,12 @@ def convert(filepath):
     df = pd.read_csv(filepath)
     new_df = convert_coordinates(df)
     base, ext = splitext(filepath)
-    base = base + "_radec"
-    path = base + extsep + ext
+    base = base.replace("_radec", "")
+    path = base + ext
     new_df.to_csv(path, index=False)
 
 
 if __name__ == "__main__":
-    import sys
-    filepath = sys.argv[1]
-    convert(filepath)
+    for phase in range(271):
+        fpath = join("data", f"RW_cluster_oscillation_{phase}_updated_radec.csv")
+        convert(fpath)
