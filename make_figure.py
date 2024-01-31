8bfc66d00395e8d4b8ce378dd600f3d35cb459b5
Carifio24 <carifio24@gmail.com>
2024-01-31 16:14:01 -0500

    Starting simple - let's make a triangulated sphere - then we can scale it up with a node for each one, and add the animations.

--- make_figure.py
+++ make_figure.py
@@ -1,12 +1,18 @@
 from os.path import join
 import pandas as pd
 import pyvista as pv
-from gltflib import GLTF, Accessor, AccessorType, Asset, BufferView, Primitive, \
-    ComponentType, GLTFModel, Node, Scene, Attributes, Mesh, Buffer
+from gltflib.gltf import GLTF
+from gltflib.gltf_resource import FileResource
+from gltflib import Accessor, AccessorType, Asset, BufferTarget, BufferView, Primitive, \
+    ComponentType, GLTFModel, Node, Scene, Attributes, Mesh, Buffer, \
+    Animation, AnimationSampler, Channel, Target
+import operator
 import struct
 
 def cluster_filepath(phase):
     return join("data", f"RW_cluster_oscillation_{phase}_updated.csv")
+
+output_directory = "out"
 
 initial_filepath = cluster_filepath(0)
 initial_df = pd.read_csv(initial_filepath)
@@ -27,8 +33,10 @@ for time in timestamps:
 
 prev_xyz = [initial_df["x"], initial_df["y"], initial_df["z"]]
 forward_differences = { pt: [] for pt in range(N_POINTS) }
+dfs = {}
 for phase in range(N_PHASES):
     df = pd.read_csv(cluster_filepath(phase))
+    dfs[phase] = df
     xyz = [df["x"], df["y"], df["z"]]
     diffs = [c - pc for c, pc in zip(xyz, prev_xyz)]
     for pt in range(N_POINTS):
@@ -48,14 +56,64 @@ time_buffer = Buffer(byteLength=len(time_bytearray), uri=time_bin)
 points_bin = "points.bin"
 points_buffer = Buffer(byteLength=len(points_bytearray), uri=points_bin)
 
+time_min = min(timestamps)
+time_max = max(timestamps)
+pt_mins = [min([point[i] for point in points]) for i in range(3)]
+pt_maxs = [max([point[i] for point in points]) for i in range(3)]
 buffers = [time_buffer, points_buffer]
-for point, bytearr in enumerate(forward_diff_bytearrays):
-    buff = Buffer(byteLength=len(bytearr), uri=f"diffs_{point}.bin")
-    buffers.append(buff)
+buffer_views = [
+    BufferView(buffer=0, byteLength=len(time_bytearray)),
+    BufferView(buffer=1, byteLength=len(points_bytearray))
+]
+accessors = [
+    Accessor(bufferView=0, componentType=ComponentType.FLOAT.value, count=N_PHASES, type=AccessorType.SCALAR.value, min=[time_min], max=[time_max]),
+    Accessor(bufferView=1, componentType=ComponentType.FLOAT.value, count=N_POINTS, type=AccessorType.VEC3.value, min=pt_mins, max=pt_maxs)
+]
+file_resources = [
+    FileResource(time_bin, data=time_bytearray),
+    FileResource(points_bin, data=points_bytearray)
+]
 
-gltf = GLTFModel(asset=Asset(version='2.0'),
-                 scenes=[Scene(nodes=list(range(N_POINTS)))],
-                 nodes=[Node(mesh=0) for _ in range(N_POINTS)],
-                 meshes=[Mesh(primitives=[Primitive(attributes=Attributes(POSITION=0))])],
-                 buffers=buffers,
+meshes = [Mesh(primitives=[Primitive(attributes=Attributes(POSITION=0))])]
+nodes = [Node(mesh=0) for _ in range(N_POINTS)]
+targets = [Target(node=idx, path="translation") for idx in range(len(nodes))]
+samplers = [AnimationSampler(input=0, interpolation="LINEAR", output=point+2) for point in range(N_POINTS)]
+channels = [Channel(target=t, sampler=0) for i, t in enumerate(targets)]
+animations = [Animation(channels=[c], samplers=[s]) for c, s in zip(channels, samplers)]
+
+samplers = []
+channels = []
+for point, bytearr in enumerate(forward_diff_bytearrays):
+    uri = f"diffs_{point}.bin"
+    buff = Buffer(byteLength=len(bytearr), uri=uri)
+    buffers.append(buff)
+    mins = []
+    maxs = []
+    for i in range(3):
+        vals = [diff[i] for diff in forward_differences[point]]
+        mins.append(min(vals))
+        maxs.append(max(vals))
+    if point == 0:
+        print(mins)
+        print(maxs)
+        print("====")
+        for diff in forward_differences[point]:
+            print(diff)
+    # The extra 2 accounts for the time and point buffers added above
+    buffer_views.append(BufferView(buffer=point+2, byteLength=len(bytearr)))
+    accessors.append(Accessor(bufferView=point+2, componentType=ComponentType.FLOAT.value, count=N_POINTS, type=AccessorType.VEC3.value, min=mins, max=maxs))
+    file_resources.append(FileResource(uri, data=bytearr))
+
+model = GLTFModel(asset=Asset(version='2.0'),
+                  scenes=[Scene(nodes=list(range(N_POINTS)))],
+                  meshes=meshes,
+                  nodes=nodes,
+                  buffers=buffers,
+                  bufferViews=buffer_views,
+                  accessors=accessors,
+                  # animations=animations
         )
+
+gltf = GLTF(model=model, resources=file_resources)
+gltf.export(join(output_directory, "test.gltf"))
+gltf.export(join(output_directory, "test.glb"))
---
+++ make_figure_v1.py
@@ -0,0 +1,102 @@
+from itertools import product
+import math
+from os.path import join
+import pandas as pd
+import pyvista as pv
+from gltflib.gltf import GLTF
+from gltflib.gltf_resource import FileResource
+from gltflib import Accessor, AccessorType, Asset, BufferTarget, BufferView, Primitive, \
+    ComponentType, GLTFModel, Node, Scene, Attributes, Mesh, Buffer, \
+    Animation, AnimationSampler, Channel, Target
+import operator
+import struct
+
+
+def cluster_filepath(phase):
+    return join("data", f"RW_cluster_oscillation_{phase}_updated.csv")
+
+
+def sphere_mesh_index(row, column, theta_resolution, phi_resolution):
+    if row == 0:
+        return 0
+    elif row == theta_resolution - 1:
+        return (theta_resolution - 2) * phi_resolution + 1
+    else:
+        return phi_resolution * (row - 1) + column + 1
+
+
+# theta is the azimuthal angle here. Sorry math folks
+def sphere_mesh(center, radius, theta_resolution=5, phi_resolution=5):
+    nonpole_thetas = [i * 2 * math.pi / theta_resolution for i in range(1, theta_resolution-1)]
+    phis = [i * math.pi - math.pi / 2 for i in range(1, phi_resolution-1)]
+    points = [(
+        center[0] + radius * math.cos(phi) * math.sin(theta),
+        center[1] + radius * math.sin(phi) * math.sin(theta),
+        center[2] + radius * math.cos(theta)
+    ) for phi, theta in product(phis, nonpole_thetas)]
+    points = [(center[0], center[1], center[2] + radius)] + points + [(center[0], center[1], center[2] - radius)]
+
+    # TODO: Make a cleaner way to handle "modular" aspect of rows
+    # Idea: Make column = column % phi_resolution in `sphere_mesh_index` ?
+    triangles = [(int(0), i, i + 1) for i in range(1, theta_resolution-1)]
+    tr, pr = theta_resolution, phi_resolution
+    triangles.append((0, theta_resolution, 1))
+    for row in range(1, theta_resolution - 2):
+        for col in range(phi_resolution - 1):
+            rc_index = sphere_mesh_index(row, col, tr, pr)
+            triangles.append((rc_index, sphere_mesh_index(row+1, col, tr, pr), sphere_mesh_index(row+1, col-1, tr, pr)))
+            triangles.append((rc_index, sphere_mesh_index(row, col+1, tr, pr), sphere_mesh_index(row+1, col, tr, pr)))
+        triangles.append((sphere_mesh_index(row, pr-1, tr, pr), sphere_mesh_index(row+1, pr-1, tr, pr), sphere_mesh_index(row+1, pr-2, tr, pr)))
+        triangles.append((sphere_mesh_index(row, pr-1, tr, pr), sphere_mesh_index(row, 0, tr, pr), sphere_mesh_index(row+1, pr-1, tr, pr)))
+        
+    row = theta_resolution - 2
+    last_index = sphere_mesh_index(theta_resolution - 1, 0, tr, pr)
+    for col in range(phi_resolution-1):
+        triangles.append((sphere_mesh_index(row, col, tr, pr), sphere_mesh_index(row, col+1, tr, pr), last_index))
+    triangles.append((sphere_mesh_index(row, pr-1, tr, pr), sphere_mesh_index(row, 0, tr, pr), last_index))
+
+    return points, triangles
+
+
+center = (0, 0, 0)
+radius = 1
+theta_resolution = 5
+phi_resolution = 5
+points, triangles = sphere_mesh(center, radius, theta_resolution=theta_resolution, phi_resolution=phi_resolution)
+
+output_directory = "out"
+
+N_POINTS = phi_resolution * (theta_resolution - 2) + 2
+arr = bytearray()
+for point in points:
+    for coord in point:
+        arr.extend(struct.pack('f', coord))
+
+triangles_offset = len(arr)
+for triangle in triangles:
+    for index in triangle:
+        arr.extend(struct.pack('I', index))
+
+buffer = Buffer(byteLength=len(arr), uri="buf.bin")
+buffer_views = [
+    BufferView(buffer=0, byteLength=triangles_offset, target=BufferTarget.ARRAY_BUFFER.value),
+    BufferView(buffer=0, byteOffset=triangles_offset, byteLength=len(arr)-triangles_offset, target=BufferTarget.ELEMENT_ARRAY_BUFFER.value)
+]
+accessors = [
+    Accessor(bufferView=0, componentType=ComponentType.FLOAT.value, count=N_POINTS, type=AccessorType.VEC3.value),
+    Accessor(bufferView=1, componentType=ComponentType.UNSIGNED_INT.value, count=len(triangles) * 3, type=AccessorType.SCALAR.value, min=[0], max=[N_POINTS-1])
+]
+file_resources = [FileResource("buf.bin", data=arr)]
+
+model = GLTFModel(
+    asset=Asset(version='2.0'),
+    scenes=[Scene(nodes=[0])],
+    nodes=[Node(mesh=0)],
+    meshes=[Mesh(primitives=[Primitive(attributes=Attributes(POSITION=0), indices=1)])],
+    buffers=[buffer],
+    bufferViews=buffer_views,
+    accessors=accessors
+)
+gltf = GLTF(model=model, resources=file_resources)
+gltf.export(join(output_directory, "test.gltf"))
+gltf.export(join(output_directory, "test.glb"))
